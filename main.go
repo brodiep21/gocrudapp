9a739e96e77a2d7916ad38bef26ecb6fa3e035fb
Brodie Peif <brodiep21@hotmail.com>
2022-03-24 19:51:57 -0400

    API working with Postman

--- main.go
+++ main.go
@@ -64,15 +64,24 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
 
-func updateMovie(id string, w http.ResponseWriter, *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	for _, item := range movies {
-		if item.ID == id {
-			
+
+	params := mux.Vars(r)
+
+	for key, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:key], movies[key+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
 		}
 	}
 }
+
 func main() {
 	r := mux.NewRouter()
 
@@ -94,6 +103,6 @@ func main() {
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
-	fmt.Println("Starting Server at port 8000\n")
-	log.Fatal(http.ListenAndServe(":8000:", r))
+	fmt.Println("Starting Server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
